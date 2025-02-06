31f3fa94c8e71258652480c7d90e01fbeaf1a102
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-02-06 12:10:41 -0400

    Correção de erro

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,27 +1,34 @@
 function contar() {
-    var num_inic = Number(document.getElementById('num_inic').value)
-    var num_fim = Number(document.getElementById('num_fim').value)
-    var num_pass = Number(document.getElementById('num_pass').value)
+    var num_inic = document.getElementById('num_inic')
+    var num_fim = document.getElementById('num_fim')
+    var num_pass = document.getElementById('num_pass')
     var res = document.getElementById('res')
-    var cont = num_inic
 
-    if (num_inic == '') {
-        res.innerText = 'Impossível contar!'
-    } else if (num_pass == '0') {
-        window.alert('Passo inválido! Será considera PASSO como 1.')
-        num_pass = 1
-        res.innerHTML = `Contando: <br>`
-        while (cont < num_fim) {
-            res.innerHTML += `${cont} &#x1F449`
-            cont += num_pass
-        }
-        res.innerHTML += `${cont} &#x1F3C1`
+    if (num_inic.value.length == 0 || num_fim.value.length == 0 || num_pass.value.length == 0) {
+        res.innerHTML = 'Inpossível contar!'
+        //window.alert('[ERRO] Preencha todos os campos!')
     } else {
-        res.innerHTML = `Contando: <br>`
-        while (cont < num_fim) {
-            res.innerHTML += `${cont} &#x1F449`
-            cont += num_pass
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(num_inic.value)
+        var f = Number(num_fim.value)
+        var p = Number(num_pass.value)
+
+        if (p <= 0) {
+            window.alert('Passo inválido! Será considerado PASSO como 1')
+            p = 1
         }
-        res.innerHTML += `${cont} &#x1F3C1`
+
+        if (i < f){
+            // Contagem crescente
+            for(var cont = i; cont <= f; cont += p) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(var cont = i; cont >= f; cont -= p) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
